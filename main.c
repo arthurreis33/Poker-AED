5b5dec835e962698413ee71feac9e078a4676da9
Arthur Reis <arthur.ars33@gmail.com>
2023-11-14 11:18:31 -0300

    second commit

--- main.c
+++ main.c
@@ -13,20 +13,62 @@ typedef struct Card{
 void enqueue(Card **head, Card **tail, int cardValue, int cardSuit){
     /* enfileira as cartas na mão de cada jogador, mesa ou cpu;
     */
+   Card *newcard = (Card*)malloc(sizeof(Card));
+
+   if(newcard != NULL){
+
+        newcard->suitCard = cardSuit;
+        newcard->valueCard = cardValue;
+
+        if(*head == NULL){
+            newcard->prev = NULL;
+            *head = newcard;
+            *tail = newcard;
+        }else{
+            (*tail)->next = newcard;
+            newcard->prev = *tail;
+            *tail = newcard;
+        }
+   }
 }
 
 /// é util que a gente crie uma função de ordenação, para caso o jogador fizer uma sequencia
-void bubbleSort(){
+void insertionSort(){
 
 }
 
 // função p concatenar a fila da mãos dos jogadores + fila das cartas da mesa
-void concatenateQueues(void){
+void concatenateQueues(Card **tail_01, Card **head_02){
+    
 
 }
 
 //função para comparar as cartas
-void compare(){
+int compare(){
+    
+    // regras para cada mão
+
+    // royal straight flush - sequencia mais alta ou seja de 9 a Ás, de mesmo naipe 
+
+    // straight flush - sequencia de mesmo naipe
+
+    // four of a kind - 4 cartas de mesmo valor
+
+    // full house - uma trinca e um par
+
+    // flush - todas as cartas do mesmo naipe
+
+    // straight - sequencia, de qualquer naipe 
+
+    // trinca - 3 cartas de mesmo valor
+
+    // dois pares - bem óbvio essa né, qualquer naipe
+
+    // um par - bem obvio essa tb
+
+    // carta mais alta - caso nenhum jogador possuir nenhuma combinação, ganha quem tiver a carta mais alta
+
+    return -1; // so pra não dar erro
 
 }
 
@@ -53,23 +95,18 @@ int main(){
         //perguntar ao jogador se ele vai entrar no jogo; custo = 5 fichas;
         //reduzir 5 fichas no montante do jogador e da cpu;
         
-        
         for (int i = 0; i<3; i++){
             enqueue(&headDealer, &tailDealer, cardValue, cardSuit); 
         }
         print("Cartas na mesa: ");//completar depois o print
-
         //rodada de aposta
         //jogador deve apostar primeiro, CPU deve cobrir a aposta;
         //colocar condição no inicio do jogo, caso a cpu esteja quebrada, termina o jogo e dá exit;
-
         //mesa 2 finais
-        for (int i = 0; i<2; i++){
-
+        for(int i = 0; i<2; i++){
+            enqueue(&headDealer, &tailDealer, cardValue, cardSuit); 
         }
-
         //rodada de aposta final
-        
         compare(); //compara os jogos
         
     }
